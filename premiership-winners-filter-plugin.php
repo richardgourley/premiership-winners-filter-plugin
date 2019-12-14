0e2751f7225a475b18a6ab6df3ca44ac0fdff9e6
unknown <codeleap@outlook.com>
2019-12-14 20:42:29 +0100

    Main plugin file added.

---
+++ premiership-winners-filter-plugin.php
@@ -0,0 +1,22 @@
+<?php
+/*
+* Plugin Name: Guitar Chord Sequence Plugin
+* Plugin URI: http://wprevs.com
+* Description: A plugin for guitarists that creates a part of a page or post, where chord sequences of different lengths can be generated at the click of a button. It can be useful for practicing the guitar or even writing songs.   
+* Author: wprevs.com
+* Version: 1.0.0
+* Author URI: http://wprevs.com
+* License: GPLv2 or later
+*/
+
+if(!defined( 'ABSPATH' )) exit;
+<?php
+/*
+* Plugin Name: Premiership Winners Filter Plugin
+* Plugin URI: http://wprevs.com
+* Description: This plugin adds details about the winners and the runners up of the English Premiership football league for the last 10 years. It provides advanced search and filter options to gain insights into points totals, goals and more.   
+* Author: wprevs.com
+* Version: 1.0.0
+* Author URI: http://wprevs.com
+* License: GPLv2 or later
+*/
