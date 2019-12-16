7b5b6b4020bdf10186a36b30554fabbc5598517d
unknown <codeleap@outlook.com>
2019-12-16 23:06:34 +0100

    Register taxonomies method added to taxonomies class.

--- classes-init/class-pwf-taxonomies-initializer.php
+++ classes-init/class-pwf-taxonomies-initializer.php
@@ -4,4 +4,30 @@ class PWF_Taxonomies_Initializer{
     public function __construct(){
         add_action( 'init', array( $this, 'register_taxonomies' ) );
     }
+
+    public function register_taxonomies(){
+        $labels_team = array(
+        "name" => __( "Team" ),
+         "singular_name" => __( "Team" ),
+        );
+        $args_team = array(
+            "label" => __( "Team" ),
+            "labels" => $labels_team,
+            "public" => true,
+            "publicly_queryable" => true,
+            "hierarchical" => true,
+            "show_ui" => false,
+            "show_in_menu" => false,
+            "show_in_nav_menus" => false,
+            "query_var" => true,
+            "rewrite" => array( 'slug' => 'team', 'with_front' => true, ),
+            "show_admin_column" => false,
+            "show_in_rest" => true,
+            "rest_base" => "team",
+            "rest_controller_class" => "WP_REST_Terms_Controller",
+            "show_in_quick_edit" => false,
+        );
+
+        register_taxonomy( 'team', array( "team" ), $args_team );
+    }
 }
