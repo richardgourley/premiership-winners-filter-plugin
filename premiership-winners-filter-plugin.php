960095191f518db13d18e1d3006fb4fbb8af2676
unknown <codeleap@outlook.com>
2019-12-16 22:47:47 +0100

    Team taxonomy test added.

--- premiership-winners-filter-plugin.php
+++ premiership-winners-filter-plugin.php
@@ -20,3 +20,30 @@ if(!defined( 'ABSPATH' )) exit;
 * Author URI: http://wprevs.com
 * License: GPLv2 or later
 */
+
+function add_test_register_taxonomy_team(){
+    $labels_team = array(
+            "name" => __( "Team" ),
+             "singular_name" => __( "Team" ),
+        );
+        $args_team = array(
+            "label" => __( "Team" ),
+            "labels" => $labels_team,
+            "public" => true,
+            "publicly_queryable" => true,
+            "hierarchical" => true,
+            "show_ui" => false,
+            "show_in_menu" => false,
+            "show_in_nav_menus" => false,
+            "query_var" => true,
+            "rewrite" => array( 'slug' => 'team', 'with_front' => true, ),
+            "show_admin_column" => false,
+            "show_in_rest" => true,
+            "rest_base" => "team",
+            "rest_controller_class" => "WP_REST_Terms_Controller",
+            "show_in_quick_edit" => false,
+        );
+        register_taxonomy( 'team', array( "team" ), $args_team );
+}
+
+add_action( 'init', 'add_test_register_taxonomy_team' );
