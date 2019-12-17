9d8c0aa21d5a2091f73a0f965b01f990bb99aa18
unknown <codeleap@outlook.com>
2019-12-17 16:59:46 +0100

    League taxonomy added.

--- classes-init/class-pwf-taxonomies-initializer.php
+++ classes-init/class-pwf-taxonomies-initializer.php
@@ -39,5 +39,29 @@ class PWF_Taxonomies_Initializer{
         );
 
         register_taxonomy( 'team', array( "team" ), $args_team );
+
+        $labels_league = array(
+        "name" => __( "League" ),
+         "singular_name" => __( "League" ),
+        );
+        $args_league = array(
+            "label" => __( "League" ),
+            "labels" => $labels_league,
+            "public" => true,
+            "publicly_queryable" => true,
+            "hierarchical" => true,
+            "show_ui" => false,
+            "show_in_menu" => false,
+            "show_in_nav_menus" => false,
+            "query_var" => true,
+            "rewrite" => array( 'slug' => 'league', 'with_front' => true, ),
+            "show_admin_column" => false,
+            "show_in_rest" => true,
+            "rest_base" => "team",
+            "rest_controller_class" => "WP_REST_Terms_Controller",
+            "show_in_quick_edit" => false,
+        );
+
+        register_taxonomy( 'league', array( "team" ), $args_league );
     }
 }
