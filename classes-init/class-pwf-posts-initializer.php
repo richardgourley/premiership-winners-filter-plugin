6b9c42d904bd30f405dd24bd1062290888c39890
unknown <codeleap@outlook.com>
2019-12-19 14:39:25 +0100

    Comments added to insert team posts method.

--- classes-init/class-pwf-posts-initializer.php
+++ classes-init/class-pwf-posts-initializer.php
@@ -5,8 +5,12 @@ class PWF_Posts_Initializer{
         
     }
 
+    /*****
+    returns null
+    Inserts all team posts for the plugin, linking posts to taxonomies and adding postmeta data
+    *****/
     public function insert_team_posts(){
-
+        
     }
 
 }
