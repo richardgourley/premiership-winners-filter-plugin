6e689184fb8b2700e6328dc697c9c923acf87e0a
unknown <codeleap@outlook.com>
2019-12-21 15:34:54 +0100

    Methods get_only_winners and get_only_runners_up added to model class.

--- classes-model/class-pwf-model.php
+++ classes-model/class-pwf-model.php
@@ -5,6 +5,46 @@ class PWF_Model{
         
     }
 
+    public function get_only_winners(){
+        $args = array(
+            'post_type'  => 'team',
+            'meta_key'   => 'points',
+            'orderby'    => 'meta_value_num',
+            'order'      => 'DESC',
+            'posts_per_page' => '-1',
+            'tax_query' => array(
+                array(
+                    'taxonomy' => 'position',
+                    'field' => 'slug',
+                    'terms' => 'winner'
+                )
+            )
+        );
+        $query = new WP_Query( $args );
+
+        return $query;
+    }
+
+    public function get_runners_up(){
+        $args = array(
+            'post_type'  => 'team',
+            'meta_key'   => 'points',
+            'orderby'    => 'meta_value_num',
+            'order'      => 'DESC',
+            'posts_per_page' => '-1',
+            'tax_query' => array(
+                array(
+                    'taxonomy' => 'position',
+                    'field' => 'slug',
+                    'terms' => 'runner-up'
+                )
+            )
+        );
+        $query = new WP_Query( $args );
+
+        return $query;
+    }
+
 
 
     
