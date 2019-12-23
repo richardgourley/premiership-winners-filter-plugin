9255b9cef6b8f9aafebc00e28565b45ea2b62f95
unknown <codeleap@outlook.com>
2019-12-23 19:21:38 +0100

    Winners lowest points added.

--- classes-model/class-pwf-model.php
+++ classes-model/class-pwf-model.php
@@ -58,6 +58,26 @@ class PWF_Model{
         return $query;
     }
 
+    public function get_winners_lowest_points(){
+        $args = array(
+            'post_type'  => 'team',
+            'meta_key'   => 'points',
+            'orderby'    => 'meta_value_num',
+            'order'      => 'ASC',
+            'posts_per_page' => '-1',
+            'tax_query' => array(
+                array(
+                    'taxonomy' => 'position',
+                    'field' => 'slug',
+                    'terms' => 'winner'
+                )
+            )
+        );
+        $query = new WP_Query( $args );
+
+        return $query;
+    }
+
 
 
     
