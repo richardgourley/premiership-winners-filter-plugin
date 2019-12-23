1b3a225c4dd19735c250389d23dac19387454614
unknown <codeleap@outlook.com>
2019-12-23 22:30:40 +0100

    get_winners_least_goals_conceded method added to model class.

--- classes-model/class-pwf-model.php
+++ classes-model/class-pwf-model.php
@@ -97,6 +97,26 @@ class PWF_Model{
 
         return $query;
     }
+
+    public function get_winners_least_goals_conceded(){
+        $args = array(
+            'post_type'  => 'team',
+            'meta_key'   => 'Goals Against',
+            'orderby'    => 'meta_value_num',
+            'order'      => 'ASC',
+            'posts_per_page' => '-1',
+            'tax_query' => array(
+                array(
+                    'taxonomy' => 'position',
+                    'field' => 'slug',
+                    'terms' => 'winner'
+                )
+            )
+        );
+        $query = new WP_Query( $args );
+
+        return $query;
+    }
     
 }
 
