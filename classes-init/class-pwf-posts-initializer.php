96dab13a25bb68c0f1d4e9d2b4891e0a63b1d49a
unknown <codeleap@outlook.com>
2020-01-10 20:36:25 +0100

    insert_team_data marked private.

--- classes-init/class-pwf-posts-initializer.php
+++ classes-init/class-pwf-posts-initializer.php
@@ -100,7 +100,7 @@ class PWF_Posts_Initializer{
         );
     }
 
-    private function insert_team_data(
+    protected function insert_team_data(
     	$team_name, $team_slug, $season, $position, $goals_for, $goals_against, $points
     ){
         $post_array = array(
