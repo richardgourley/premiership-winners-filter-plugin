05cb118b57cda69f3bacd1af2e77feff208653a6
unknown <codeleap@outlook.com>
2020-02-21 12:39:23 +0100

    Class added to table outer div.

--- views/table.php
+++ views/table.php
@@ -1,33 +1,42 @@
 <div class="pwf-intro-text">
   <h4>We took all the data of Premiership winners and runners up over the last <?php echo $number_of_seasons; ?> years. Filter the results from  the drop-down menu below.
   </h4>
-  <p><?php //echo PLUGIN_ROOT; ?></p>
-  <?php //echo esc_url( get_the_permalink() ); ?>
 </div>
 
 <div class="pwf-flexbox-grid">
   <div>
-    <p>Filter the results</p>
+    <p>Search by most and least points</p>
     <div class="pwf-select-menu">
-    <form action="<?php esc_url( get_the_permalink() ) ?>" method="post">
-    <?php wp_nonce_field( 'pwf_form_action', 'pwf_form_nonce' ); ?>
-    <select name="pwf-options">
-    <?php echo $form_options; ?>
-    </select>
+      <form action="<?php esc_url( get_the_permalink() ) ?>" method="post">
+      <?php wp_nonce_field( 'pwf_form_action', 'pwf_form_nonce' ); ?>
+      <select name="pwf-options">
+      <?php echo $form_options_search_by_points; ?>
+      </select>
     </div>
     <div class="pwf-form-button">
-    <input type="submit" value="Filter results">
+      <input type="submit" value="Filter results">
+    </div>
+    </form>
+
+    <p>Search by most and least goals</p>
+    <div class="pwf-select-menu">
+      <form action="<?php esc_url( get_the_permalink() ) ?>" method="post">
+      <?php wp_nonce_field( 'pwf_form_action', 'pwf_form_nonce' ); ?>
+      <select name="pwf-options">
+      <?php echo $form_options_search_by_goals; ?>
+      </select>
+    </div>
+    <div class="pwf-form-button">
+      <input type="submit" value="Filter results">
     </div>
     </form>
   </div>
 
-  <div>
+  <div class="pwf-display-table-div">
     <div class="pwf-display-table">
       <table class="form-table">
-        <tr>
-          <th>Season</th>
-          <th>Position</th>
-          <th>Team</th>
+        <tr class="pwf-table-heading">
+          <th>Team, Season, Position</th>
           <th>Goals For</th>
           <th>Goals Against</th>
           <th>Points</th>
@@ -35,9 +44,11 @@
         
           <?php foreach( $results->posts as $result): ?>
             <tr>
-            <td><?php echo esc_html( get_the_terms( $result->ID, 'season' )[0]->name ); ?></td>
-            <td><?php  echo esc_html( get_the_terms( $result->ID, 'position' )[0]->name ); ?></td>
-            <td><?php  echo $result->post_title; ?></td>
+            <td>
+              <strong><?php  echo $result->post_title; ?></strong><br>
+              <?php echo esc_html( get_the_terms( $result->ID, 'season' )[0]->name ); ?><br>
+              <?php  echo esc_html( get_the_terms( $result->ID, 'position' )[0]->name ); ?><br>
+            </td>
             <td><?php  echo esc_html( get_post_meta( $result->ID, 'Goals For', true ) ); ?></td>
             <td><?php  echo esc_html( get_post_meta( $result->ID, 'Goals Against', true ) ); ?></td>
             <td><strong><?php echo esc_html( get_post_meta( $result->ID, 'Points', true ) ); ?></strong></td>
