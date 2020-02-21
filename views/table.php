c07434f4780271ffddcd32603d509a7e21d678c5
unknown <codeleap@outlook.com>
2020-02-21 15:50:07 +0100

    Option names in menu match controller and model method names.

--- views/table.php
+++ views/table.php
@@ -10,28 +10,49 @@
       <form action="<?php esc_url( get_the_permalink() ) ?>" method="post">
       <?php wp_nonce_field( 'pwf_form_action', 'pwf_form_nonce' ); ?>
       <select name="pwf-options">
-      <option value="winners_runners_up_highest_points">Winners & runners-up - Highest Points</option>
-      <option value="winners_highest_points">Winners - Highest Points</option>
-      <option value="runners_up_highest_points">Runners-Up - Highest Points</option>
+        <option value="winners_runners_up_highest_points">Winners & runners-up - Highest Points</option>
+        <option value="winners_highest_points">Winners - Highest Points</option>
+        <option value="runners_up_highest_points">Runners-Up - Highest Points</option>
+        <option value="winners_lowest_points">Winners - Lowest Points</option>
+        <option value="runners_up_lowest_points">Runners-Up - Lowest Points</option>
       </select>
     </div>
-    <div class="pwf-form-button">
-      <input type="submit" value="Filter by points">
-    </div>
-    </form>
+      <div class="pwf-form-button">
+        <input type="submit" value="Filter by points">
+      </div>
+      </form>
 
     <p>Filter by most and least goals</p>
     <div class="pwf-select-menu">
       <form action="<?php esc_url( get_the_permalink() ) ?>" method="post">
       <?php wp_nonce_field( 'pwf_form_action', 'pwf_form_nonce' ); ?>
       <select name="pwf-options">
-      <?php //echo $form_options_search_by_goals; ?>
+        <option value="winners_most_goals">Winners - Most goals</option>
+        <option value="runners_up_most_goals">Runners-Up - Most goals</option>
+        <option value="winners_least_goals">Winners - Least goals</option>
+        <option value="runners_up_least_goals">Runners-Up - Least goals</option>
+        <option value="winners_least_goals_conceded">Winners - Least goals conceded</option>
+        <option value="runners_up_least_goals_conceded">Runners-Up - Least goals conceded</option>
+
       </select>
     </div>
-    <div class="pwf-form-button">
-      <input type="submit" value="Filter by goals">
+      <div class="pwf-form-button">
+        <input type="submit" value="Filter by goals">
+      </div>
+      </form>
+
+    <p>Filter by averages</p>
+    <div class="pwf-select-menu">
+      <form action="<?php esc_url( get_the_permalink() ) ?>" method="post">
+      <?php wp_nonce_field( 'pwf_form_action', 'pwf_form_nonce' ); ?>
+      <select name="pwf-options">
+        <option value="average_points">Average points</option>
+      </select>
     </div>
-    </form>
+      <div class="pwf-form-button">
+        <input type="submit" value="Filter by averages">
+      </div>
+      </form>
   </div>
 
   <div class="pwf-display-table-div">
