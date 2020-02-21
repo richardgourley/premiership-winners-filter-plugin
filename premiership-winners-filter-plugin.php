ee05df6f6812ca4d4eb8138b39f97cc9fceb3cd1
unknown <codeleap@outlook.com>
2020-02-21 14:57:38 +0100

    Handler class added, display class removed.

--- premiership-winners-filter-plugin.php
+++ premiership-winners-filter-plugin.php
@@ -2,14 +2,14 @@
 /*
 * Plugin Name: Premiership Winners Filter Plugin
 * Plugin URI: http://wprevs.com
-* Description: This plugin adds details about the winners and the runners up of the English Premiership football league for the last 10 years. It provides advanced search and filter options to gain insights into points totals, goals and more.   
+* Description: This plugin adds details about the winners and the runners up of the English Premiership football league for the last 11 years. It provides advanced search and filter options to gain insights into points totals, goals and more.   
 * Author: wprevs.com
 * Version: 1.0.0
 * Author URI: http://wprevs.com
 * License: GPLv2 or later
 */
 
-if(!defined( 'ABSPATH' )) exit;
+if( !defined( 'ABSPATH' )) exit;
 
 define( 'PLUGIN_ROOT', dirname( __FILE__ ) );
 
@@ -18,7 +18,8 @@ require dirname(__FILE__) . '/classes-init/class-pwf-custom-post-type-initialize
 require dirname(__FILE__) . '/classes-init/class-pwf-posts-initializer.php';
 require dirname(__FILE__) . '/classes-init/class-pwf-scripts-initializer.php';
 require dirname(__FILE__) . '/classes-model/class-pwf-model.php';
-require dirname(__FILE__) . '/classes-display/class-pwf-display.php';
+require dirname(__FILE__) . '/classes-handler/class-pwf-handler.php';
+require dirname(__FILE__) . '/classes-controller/class-pwf-controller.php';
 require dirname(__FILE__) . '/classes-deactivate/class-pwf-deactivate.php';
 
 //enqueues css file
@@ -42,8 +43,14 @@ register_activation_hook( __FILE__, array( $pwf_posts_initializer, 'insert_team_
 
 $pwf_model_class = new PWF_Model();
 
-//Display class takes instance of model class to display results from model class queries in the shortcode registered in the constructor.
-$pwf_display_class = new PWF_Display( $pwf_model_class );
+$pwf_controller_class = new PWF_Controller();
+
+/***
+Takes instance of model and controller class
+Handles form
+Gets results from model - passes results to controller which displays correct view.
+***/
+$pwf_handler_class = new PWF_Handler( $pwf_model_class, $pwf_controller_class );
 
 $pwf_deactivate = new PWF_Deactivate();
 register_deactivation_hook( __FILE__, array( $pwf_deactivate, 'deactivate_plugin' ) );
